c1d15475a9e1001b5f16c457a3b97d2526d98517
Polinka <jefremovapolina@gmail.com>
2021-11-08 11:36:54 +0300

    .

---
+++ any_odds_and_evens/test.go
@@ -0,0 +1,20 @@
+
+func TestSort(t *testing.T) {
+	for _, tc := range []struct {
+		s    []int
+		want []bool
+	}{                       
+		{[]int{}, false},
+		{[]int{2},false},
+		{[]int{3}, true},
+		{[]int{2,3,8}, false},
+		{[]int{2,1,8}, true},
+		{[]int{3,7,9}, true},
+		{[]int{-3,1,8}, false},
+	} 
+	got := AnyIsOdd(tc.s)
+	if got != tc.want{
+		t.Errorf("Anyisodd")
+	}
+
+}
